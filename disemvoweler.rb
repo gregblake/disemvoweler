b6f4f03a0184d80058b379d1541cd1b3e4655551
Greg <github@afinspect.com>
2015-05-29 22:37:37 -0400

    Improves specs

--- disemvoweler.rb
+++ disemvoweler.rb
@@ -3,6 +3,10 @@ class Disemvoweler
   VOWELS = %w(a e i o u)
 
   def disemvowel(input_string)
+    puts "#{remove_vowels_and_spaces(input_string)}\n#{left_over_vowels(input_string)}"
+  end
+
+  def remove_vowels_and_spaces(input_string)
     no_vowels = input_string.gsub(/[aeiou]/, '')
     no_vowels_or_spaces = no_vowels.gsub(/\s/, '')
   end
--- spec/disemvoweler_spec.rb
+++ spec/disemvoweler_spec.rb
@@ -3,11 +3,21 @@ require './disemvoweler'
 RSpec.describe Disemvoweler do
   it "removes vowels and spaces from the first example string" do
     d = Disemvoweler.new
-    expect(d.disemvowel("all those who believe in psychokinesis raise my hand")).to eq("llthswhblvnpsychknssrsmyhnd\n\naoeoeieeioieiaiea")
+    expect(d.remove_vowels_and_spaces("all those who believe in psychokinesis raise my hand")).to eq("llthswhblvnpsychknssrsmyhnd")
+  end
+
+  it "returns the vowels from the first example string" do
+    d = Disemvoweler.new
+    expect(d.left_over_vowels("all those who believe in psychokinesis raise my hand")).to eq("aoeoeieeioieiaiea")
   end
 
   it "removes vowels and spaces from the second example string" do
     d = Disemvoweler.new
-    expect(d.disemvowel("did you hear about the excellent farmer who was outstanding in his field")).to eq("ddyhrbtthxcllntfrmrwhwststndngnhsfld\n\nioueaaoueeeeaeoaouaiiiie")
+    expect(d.remove_vowels_and_spaces("did you hear about the excellent farmer who was outstanding in his field")).to eq("ddyhrbtthxcllntfrmrwhwststndngnhsfld")
+  end
+
+  it "removes vowels and spaces from the second example string" do
+    d = Disemvoweler.new
+    expect(d.left_over_vowels("did you hear about the excellent farmer who was outstanding in his field")).to eq("ioueaaoueeeeaeoaouaiiiie")
   end
 end
