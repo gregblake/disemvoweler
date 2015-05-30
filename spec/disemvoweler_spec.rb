417470e53ba878266ef5d1a4c42047a82b3635b2
Greg <github@afinspect.com>
2015-05-29 21:32:39 -0400

    Second failing spec

--- spec/disemvoweler_spec.rb
+++ spec/disemvoweler_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Disemvoweler do
     expect(d.disemvowel("all those who believe in psychokinesis raise my hand")).to eq("llthswhblvnpsychknssrsmyhnd")
     expect(d.left_over_vowels).to eq("aoeoeieeioieiaiea")
   end
+
+  it "removes vowels and spaces from the second example string" do
+    d = Disemvoweler.new
+    expect(d.disemvowel("did you hear about the excellent farmer who was outstanding in his field")).to eq("ddyhrbtthxcllntfrmrwhwststndngnhsfld")
+    expect(d.left_over_vowels).to eq("ioueaaoueeeeaeoaouaiiiie")
+  end
 end
