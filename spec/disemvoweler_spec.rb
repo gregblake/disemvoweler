d53df8ed8b7caa15a6fd2ab62d3ec2d5184c3717
Greg <github@afinspect.com>
2015-05-29 22:18:43 -0400

    Modifies specs to more accurately match the sample outputs formatting on the code challenge.

--- spec/disemvoweler_spec.rb
+++ spec/disemvoweler_spec.rb
@@ -3,13 +3,11 @@ require './disemvoweler'
 RSpec.describe Disemvoweler do
   it "removes vowels and spaces from the first example string" do
     d = Disemvoweler.new
-    expect(d.disemvowel("all those who believe in psychokinesis raise my hand")).to eq("llthswhblvnpsychknssrsmyhnd")
-    expect(d.left_over_vowels"all those who believe in psychokinesis raise my hand").to eq("aoeoeieeioieiaiea")
+    expect(d.disemvowel("all those who believe in psychokinesis raise my hand")).to eq("llthswhblvnpsychknssrsmyhnd\n\naoeoeieeioieiaiea")
   end
 
   it "removes vowels and spaces from the second example string" do
     d = Disemvoweler.new
-    expect(d.disemvowel("did you hear about the excellent farmer who was outstanding in his field")).to eq("ddyhrbtthxcllntfrmrwhwststndngnhsfld")
-    expect(d.left_over_vowels("did you hear about the excellent farmer who was outstanding in his field")).to eq("ioueaaoueeeeaeoaouaiiiie")
+    expect(d.disemvowel("did you hear about the excellent farmer who was outstanding in his field")).to eq("ddyhrbtthxcllntfrmrwhwststndngnhsfld\n\nioueaaoueeeeaeoaouaiiiie")
   end
 end
